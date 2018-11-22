9ab1867d423ed2ce64409f9c9b2c160a9e0b8510
Thomas Gracie <tgracie030@gmail.com>
2018-11-22 14:09:42 +0000

    We have a DECK

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        this.deck = new ArrayList<>();
+    }
+
+    public int cardCount() {
+        return this.deck.size();
+    }
+
+//    public void fillDeck(){
+////        SuitType suit = new SuitType;
+////        for (suit : SuitType){
+////            Rank rank = new Rank;
+//            for(Rank rank : Rank.values()){
+//                Card card = new Card(SuitType.CLUBS, rank);
+//                this.deck.add(card);
+//            }
+//        }
+////    }
+
+    public void fillDeck(){
+        // need to loop through suitTypes
+            for(SuitType suit : SuitType.values()){
+                // for each suit type loop through the Rank enum
+                for(Rank rank : Rank.values()){
+                    // make a card with that Rank and that SuitType
+                    Card card = new Card(suit, rank);
+                    // add it to the deck
+                    this.deck.add(card);
+                }
+            }
+    }
 }
